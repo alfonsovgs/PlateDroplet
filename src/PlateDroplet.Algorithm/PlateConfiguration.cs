d6127588850c3b2da4ec6f0a963a26b9181753a8
Alfonso <vc.alfonso@hotmail.com>
2020-08-11 22:24:49 -0500

    File renamed and class unnecessary deleted

---
+++ src/PlateDroplet.Algorithm/PlateConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace PlateDroplet.Algorithm
+{
+    public class PlateConfiguration : IPlateDropletConfiguration
+    {
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
+        public PlateConfiguration(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+        }
+    }
+}
